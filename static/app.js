29ff2a203e79a7c31489f59afd6ce281e48a1c78
PC Berno <PC Berno@BERNO>
2026-08-07 12:47:46 +0200

    aggiunto la richiesta di permesso per il microfono, e l'audioBlob dove si inseriscono tutti i frame dell'audio

--- static/app.js
+++ static/app.js
@@ -16,15 +16,55 @@ recordButton.addEventListener("click", toggleRecording);
 async function toggleRecording() {
     if (isRecording == false) {
 
+        try{
+            // Chiediamo il permesso
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true});
+        
+            // Creiamo il registratore e svuotiamo la memoria dei pezzettini
+            mediaRecorder = new MediaRecorder(stream);
+            audioChuncks = [];
+            
+            //Comando per raccogliere l'audio mentre parli
+
+            mediaRecorder.ondataavailable = (event) => {
+                audioChuncks.push(event.data);
+            };
+
+            // Che fa il registratore quando si ferma?
+         mediaRecorder.onstop = () => {
+            // Raccoglie tutti i pezzettini e li fonde in un unico file audio .webm
+            const audioBlob = new Blob(audioChuncks, {type: "audio/webm" });
+            
+            console.log("File audio creato! Dimensione:", audioBlob.size, "byte");
+
+            // Spenge il microfono di Windows/Mac
+            stream.getTracks().forEach(track => track.stop());
+         };
+        
+        // Registrazione On
+
+        mediaRecorder.start();
         isRecording = true;
+
+        // aggiornamento grafica Pulsante
+
         console.log("Inizio registrazione...");
 
         recordButton.textContent=" Registrazione in corso..";
-        recordButton.style.backgroundColor = "#c039"
+        recordButton.style.backgroundColor = "#c039";
         recordButton.style.color = "white";
+
+        }
+        catch (err) {
+        //Se non dai l'autorizzazione del microfono
+
+        alert("Per registrare, devi concedere i permessi di usare il microfono al browser!");
+        console.error("Errore microfono:", err);
+        }
     }
     else {
         
+        mediaRecorder.stop();
         isRecording = false;
         console.log("Fine registrazione");
 
